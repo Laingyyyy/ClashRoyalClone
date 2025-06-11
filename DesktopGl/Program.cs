5e271781f9c7fb69aa3f150b28b394abb1f3bc5f
laingyy <laing1324@gmail.com>
2025-06-11 18:06:55 +0100

    Initial commit

---
+++ DesktopGl/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Core;
+
+namespace DesktopGl;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        using var game = new Game1();
+        game.Run();
+    }
+}
